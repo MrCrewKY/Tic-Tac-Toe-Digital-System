e8911227da47d5581f06793535ba3910a97e44ca
Henry Lim <limhenry0065@gmail.com>
2024-02-14 18:10:35 +0800

    Update and rename winner_case.v to Datapath/Datapath Submodules/winner_case.v

--- Datapath/Datapath Submodules/winner_case.v
+++ Datapath/Datapath Submodules/winner_case.v
@@ -11,10 +11,10 @@ module winner_case(p1,p2,s1,s2);//p1=player1, p2=player2, s=result
 					9'b111xxxxxx: s1 = 1'b1;//rows
 					9'bxxx111xxx: s1 = 1'b1;//rows
 					9'bxxxxxx111: s1 = 1'b1;//rows
-               9'bxx1xx1xx1: s1 = 1'b1;//columns
+               				9'bxx1xx1xx1: s1 = 1'b1;//columns
 					9'bx1xx1xx1x: s1 = 1'b1;//columns
 					9'b1xx1xx1xx: s1 = 1'b1;//columns
-               9'bxx1x1x1xx: s1 = 1'b1;//diagonals
+               				9'bxx1x1x1xx: s1 = 1'b1;//diagonals
 					9'b1xxx1xxx1: s1 = 1'b1;//diagonals
 					default: s1 = 1'b0;
 			endcase
@@ -23,10 +23,10 @@ module winner_case(p1,p2,s1,s2);//p1=player1, p2=player2, s=result
 					9'b111xxxxxx: s2 = 1'b1;//rows
 					9'bxxx111xxx: s2 = 1'b1;//rows
 					9'bxxxxxx111: s2 = 1'b1;//rows
-               9'bxx1xx1xx1: s2 = 1'b1;//columns
+               				9'bxx1xx1xx1: s2 = 1'b1;//columns
 					9'bx1xx1xx1x: s2 = 1'b1;//columns
 					9'b1xx1xx1xx: s2 = 1'b1;//columns
-               9'bxx1x1x1xx: s2 = 1'b1;//diagonals
+               				9'bxx1x1x1xx: s2 = 1'b1;//diagonals
 					9'b1xxx1xxx1: s2 = 1'b1;//diagonals
 					default: s2 = 1'b0;
 			endcase
